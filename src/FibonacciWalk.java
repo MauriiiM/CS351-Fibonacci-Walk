457d4b6b9bc1880d71f3326e9622023a2bdd6591
Mauricio Monsivais <mmonsivais@unm.edu>
2016-10-27 13:24:09 -0600

    start

---
+++ src/FibonacciWalk.java
@@ -0,0 +1,18 @@
+/**
+ * Created by mmonsivais on 10/27/16.
+ */
+public class FibonacciWalk
+{
+  private static Walker walker1, walker2;
+
+
+  public static void main(String args[])
+  {
+//    walker1 = new Walker();
+  }
+
+  private class Walker extends Thread
+  {
+
+  }
+}
