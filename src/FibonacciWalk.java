5c0a52bbeeff86c6d6e35567491259d4b8f02c9d
Mauricio Monsivais <mmonsivais@unm.edu>
2016-10-27 16:18:51 -0600

    start

--- src/FibonacciWalk.java
+++ src/FibonacciWalk.java
@@ -3,16 +3,48 @@
  */
 public class FibonacciWalk
 {
-  private static Walker walker1, walker2;
-
-
   public static void main(String args[])
   {
-//    walker1 = new Walker();
+    Worker A = new Worker("A");
+    A.start();
+    Worker B = new Worker("B");
+    B.start();
+
+    for (int i = 0; i < 10; i++)
+    {
+      try
+      {
+        Thread.sleep(2000);
+        System.out.println("Sleeping");
+      }
+      catch (InterruptedException e)
+      {
+        System.err.println("Sleep interrupted");
+      }
+      System.out.printf("%s: %d + %d = %d", A.NAME, A.y, A.x, A.z);
+      System.out.printf("%s: %d + %d = %d", B.NAME, B.y, B.x, B.z);
+    }
   }
 
-  private class Walker extends Thread
-  {
 
+  private class Worker extends Thread
+  {
+    private final String NAME; //Name of thread
+    private long step = 0; //Steps since start.
+    private long z; // fib(step)
+    private long y = 1; // fib(step-1)
+    private long x = 1; // fib(step-2)
+
+    Worker(String name)
+    {
+      NAME = name;
+    }
+
+    @Override
+    public void run()
+    {
+      step++;
+      z = y + x;
+    }
   }
 }
